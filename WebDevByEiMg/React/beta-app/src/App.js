001877c4e7304c71f985518a73710d18c41c145e
JiraiyaOgata <thuriyac4@gmail.com>
2022-12-01 12:58:15 +0630

    adding redux

--- WebDevByEiMg/React/beta-app/src/App.js
+++ WebDevByEiMg/React/beta-app/src/App.js
@@ -130,40 +130,25 @@ import React from 'react';
 //         </div>
 //     )
 // }
-import 'bootstrap/dist/css/bootstrap.css';
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
+const MyContxt = React.createContext('Hello context');
+const App = props => (<Header />)
 
-
-function BasicExample() {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+const Header = props => {
+    console.log(`The value of props from Header is ${props}`);
+    return (
+        <Title  />
+    )
 }
 
-export default BasicExample;
+const Title = props => {
+    const name = React.useContext(MyContxt);
+    return  <p >{name}</p>
+    
+}
+
+export default App;
+
+
 
 
 
