1dec61307eda186da4b4ecb472a7f225f9e07fc0
JiraiyaOgata <thuriyac4@gmail.com>
2022-12-02 15:09:47 +0630

    adding calculator react project

---
+++ WebDevByEiMg/React/calculator/src/App.js
@@ -0,0 +1,23 @@
+import {Button, Alert} from 'react-bootstrap'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = props => {
+    return (
+        <div className='body'>
+            <div className='content-container'>
+                <div className='display'>
+                    <div>Output 1</div>
+                    <div>Output 2</div>
+                </div>
+
+                <div className='btn-container'>
+                    <Button variant='outline-warning'>1</Button>
+                    <Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button><Button variant='outline-warning'>1</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App;
